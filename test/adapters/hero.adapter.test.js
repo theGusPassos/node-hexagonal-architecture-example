645f7ff9fded6c9ef120387c09e71d7cd85f3566
theguspassos <theguspassos@gmail.com>
2021-07-10 20:21:36 -0300

    add tests for adapters

--- test/adapters/hero.adapter.test.js
+++ test/adapters/hero.adapter.test.js
@@ -1,6 +1,36 @@
 import heroAdapter from '../../src/adapters/hero.adapter'
+import faker from 'faker'
+import { expect } from '@jest/globals'
+import { v4 as uuidv4 } from 'uuid'
 
-test('adds 1 + 2 to equal 3', () => {
-  heroAdapter.toExternal(null)
-  expect(3).toBe(3)
-})
+describe('hero.adapter', () =>
+  test('toInternal', () => {
+    const external = {
+      name: faker.random.word(),
+      level: faker.datatype.number(100),
+    }
+
+    const internal = heroAdapter.toInternal(external)
+
+    expect(internal).toMatchObject(external)
+    expect(internal.id).toBeDefined()
+    expect(internal.createdAt).toBeInstanceOf(Date)
+  }),
+
+test('toExternal', () => {
+  const internal = {
+    id: uuidv4(),
+    name: faker.random.word(),
+    level: faker.datatype.number(100),
+    createdAt: new Date(),
+  }
+
+  const external = heroAdapter.toExternal(internal)
+
+  expect(external).toMatchObject({
+    id: internal.id,
+    name: internal.name,
+    level: internal.level,
+  })
+}),
+)
